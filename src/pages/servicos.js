3ceb6616e201017cecd0f2517e61149348d24b32
constancacc <constancamfcorreia@gmail.com>
2024-02-08 12:45:18 +0000

    ginasio

--- src/pages/servicos.js
+++ src/pages/servicos.js
@@ -3,10 +3,15 @@ import Footer from '../components/Footer.js';
 
 import precario from '../img/documentos/PRECÁRIO.pdf';
 import atualizacao from '../img/documentos/Atualização da Politica de Funcionamento do TCFF.pdf';
+import ginasio from '../img/documentos/POLÍTICA DE FUNCIONAMENTO DO GINÁSIO.pdf';
 
 import atencao from '../img/servicos/atencao.svg';
 import img1 from '../img/servicos/servicos1.png';
 import mapa from '../img/servicos/mapa.png';
+import balneario from '../img/servicos/balneario.jpeg';
+import ginasio1 from '../img/servicos/ginasio1.png'; 
+import ginasio2 from '../img/servicos/ginasio2.png'; 
+import ginasio3 from '../img/servicos/ginasio3.png';
 
 function Servicos() {
     return (
@@ -59,7 +64,7 @@ function Servicos() {
             </div>
             <div className="gutter"></div>
           </div>
-          <div className="row altura" style={{marginTop: "5vh"}}>
+          <div className="row" style={{marginTop: "15vh", marginBottom: "15vh"}}>
             <div className="gutter"></div>
             <div className="col-lg-6">
               <h1 className="subtitulo section">Balneários</h1>
@@ -70,17 +75,38 @@ function Servicos() {
             </div>
             <div className="col-lg-1"></div>
             <div className="col-lg-5" style={{padding: "0"}}>
-              <img className="section" src={img1} alt="servicos1" style={{width: "37.5vw", borderRadius: "1.31238rem"}} />
+              <img className="section" src={balneario} alt="servicos1" style={{width: "37.5vw", borderRadius: "1.31238rem"}} />
 
-              <div className="botao-download botao_aulas">
-                    <a className="white" href={precario} download>
-                      Preçário de Aluger de Campos
-                    </a>
-              </div>
+      
 
             </div>
             <div className="gutter"></div>
           </div>
+
+          <div className="row altura">
+            <div className="gutter"></div>
+            <div className="col-lg-5" style={{padding:0}}>
+              <img className="section" src={ginasio1} alt="ginasio" style={{width: "37.5vw", borderRadius: "1.31238rem"}} />
+              <div className="botao-download botao_aulas">
+                    <a className="white" href={ginasio} download>
+                      Guia de Utilização
+                    </a>
+              </div>
+              <img className="section" src={ginasio2} alt="ginasio" style={{width: "37.5vw", borderRadius: "1.31238rem"}} />
+              
+            </div>
+            <div className="col-lg-1"></div>
+            <div className="col-lg-6" style={{padding:0}}>
+              <h1 className="subtitulo section">Ginásio</h1>
+              <p className="subtexto">A última novidade do nosso clube!</p>
+              <p className="subtexto">Este ginásio, além do seu espaço amplo, possui uma passadeira, elíptica e bicicleta.
+              Temos, também, uma escolha variada de pesos e instrumentos para alcançar a sua condição física de sonho.</p>
+              <p className="subtexto">Para usufruir torne-se sócio ou inscreva-se numa das nossas escolas!</p>
+              <img src={ginasio3} alt="ginasio" style={{marginTop: "8vh", width: "45vw", borderRadius: "1.31238rem"}} />
+            </div>
+            <div className="gutter"></div>
+          </div>
+
          </div>
          <Footer/>
       </div>
